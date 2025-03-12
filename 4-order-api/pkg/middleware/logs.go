127be140db0dc44c0d57f4cada646916f11c957c
Ostrovskiy Konstantin <dev@ostkost.ru>
2025-03-12 17:05:50 +0300

    Refactor logger

--- 4-order-api/pkg/middleware/logs.go
+++ 4-order-api/pkg/middleware/logs.go
@@ -6,32 +6,42 @@ import (
 	"time"
 )
 
+// Logger возвращает middleware для логирования HTTP запросов.
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.SetFormatter(&log.JSONFormatter{})
 		start := time.Now()
 		wrapper := &WrapperWriter{
 			ResponseWriter: w,
 			StatusCode:     http.StatusOK,
 		}
+
 		next.ServeHTTP(wrapper, r)
-		requestLogger := log.WithFields(log.Fields{
-			"statusCode": wrapper.StatusCode,
-			"method":     r.Method,
-			"url":        r.URL.String(),
-			"duration":   time.Since(start),
-		})
-		switch wrapper.StatusCode {
-		case http.StatusOK:
-			requestLogger.Info("OK")
-		case http.StatusNotFound:
-			requestLogger.Warn("not found")
-		case http.StatusInternalServerError:
-			requestLogger.Error("internal server error")
-		case http.StatusUnauthorized:
-			requestLogger.Warn("unauthorized")
-		case http.StatusForbidden:
-			requestLogger.Warn("request forbidden")
-		}
+
+		logRequest(wrapper, r, start)
 	})
+}
+
+// logRequest логирует информацию о запросе.
+func logRequest(w *WrapperWriter, r *http.Request, start time.Time) {
+	requestLogger := log.WithFields(log.Fields{
+		"statusCode": w.StatusCode,
+		"method":     r.Method,
+		"url":        r.URL.String(),
+		"duration":   time.Since(start),
+	})
+
+	switch w.StatusCode {
+	case http.StatusOK:
+		requestLogger.Info("OK")
+	case http.StatusNotFound:
+		requestLogger.Warn("Not found")
+	case http.StatusInternalServerError:
+		requestLogger.Error("Internal server error")
+	case http.StatusUnauthorized:
+		requestLogger.Warn("Unauthorized")
+	case http.StatusForbidden:
+		requestLogger.Warn("Request forbidden")
+	default:
+		requestLogger.Info("Request processed")
+	}
 }
